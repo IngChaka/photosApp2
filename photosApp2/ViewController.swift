72d2a294d3517ae29c61797ebc703ae7a453c5dd
Carlos Gomez <ingcarlosgomez@msn.com>
2020-07-25 16:06:04 -0700

    Initial Commit

---
+++ photosApp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  photosApp2
+//
+//  Created by user177342 on 7/25/20.
+//  Copyright © 2020 IngChaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

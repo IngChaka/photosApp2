a578c80e83a7d298044da07cb1f89e41d95f45a3
Carlos Gomez <ingcarlosgomez@msn.com>
2020-07-25 16:50:41 -0700

    Images load and auto resize

--- photosApp2/ViewController.swift
+++ photosApp2/ViewController.swift
@@ -7,14 +7,59 @@
 //
 
 import UIKit
+import Photos
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UINavigationControllerDelegate {
 
+    var myCollectionView: UICollectionView!
+    var imageArray=[UIImage]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.title = "Photos"
+        
+        let layout = UICollectionViewFlowLayout()
+        
+        myCollectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        myCollectionView.delegate=self
+        myCollectionView.dataSource=self
+        myCollectionView.register(PhotoItemCell.self, forCellWithReuseIdentifier: "Cell")
+        myCollectionView.backgroundColor=UIColor.white
+        self.view.addSubview(myCollectionView)
+        
+        myCollectionView.autoresizingMask = UIView.Autoresizing(rawValue:
+            UIView.Autoresizing.RawValue(UInt8(UIView.Autoresizing.flexibleWidth.rawValue) |
+                UInt8(UIView.Autoresizing.flexibleHeight.rawValue)))
+        
+        grabPhotos()
+        
     }
 
 
+}
+
+class PhotoItemCell: UICollectionViewCell {
+    var img = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        img.contentMode = .scaleAspectFill
+        img.clipsToBounds=true
+        self.addSubview(img)
+        
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        img.frame = self.bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    }
 }
 
